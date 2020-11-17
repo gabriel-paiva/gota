70fcf6dd901161fa301a90eb02e1c5f42ab69d8e
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2020-11-17 16:32:08 -0300

    Criando estrutura basica da homepage e navbar

---
+++ components/Navbar/index.js
@@ -0,0 +1,12 @@
+import styles from '../../styles/Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <header className={styles.navbar}>
+      <h1>LOGO</h1>
+      <h1>Calculadora de Tarifas de Água</h1>
+      <h1>Ajuda</h1>
+      <h1>Login</h1>
+    </header>
+  );
+}
