675f599d958e017310939e9c7c401179995f45aa
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2020-11-18 12:20:43 -0300

    #5 Criando esqueleto do formulario da calculadora

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,12 +1,11 @@
+import Image from 'next/image';
+
 import styles from '../../styles/Navbar.module.css';
 
 export default function Navbar() {
   return (
     <header className={styles.navbar}>
-      <h1>LOGO</h1>
-      <h1>Calculadora de Tarifas de Água</h1>
-      <h1>Ajuda</h1>
-      <h1>Login</h1>
+      <Image src="/gota.svg" alt="Logo Gota" width={130} height={74.63} />
     </header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,30 @@ export default function Home() {
       </Head>
       <Navbar />
       <div className="container">
-        <h1>Conteúdo da Página</h1>
-        <h1>Outro conteúdo</h1>
-        <h1>Mais um conteúdo</h1>
+        <h1>Calculadora de Tarifas de Água</h1>
+        <form>
+          <label htmlFor="regiao">Escolha uma região:</label>
+          <select name="regiao" id="regiao">
+            <option value="DF">Distrito Federal</option>
+            <option value="GO">Goiás</option>
+            <option value="MS">Mato Grosso do Sul</option>
+          </select>
+
+          <label htmlFor="empresa">Escolha uma empresa de saneamento:</label>
+          <select name="empresa" id="empresa">
+            <option value="caesb">CAESB</option>
+            <option value="saneago">SANEAGO</option>
+            <option value="sanesul">SANESUL</option>
+          </select>
+
+          <label htmlFor="residencia">Escolha um tipo de residência:</label>
+          <select name="residencia" id="residencia">
+            <option value="padrao">Padrão</option>
+          </select>
+
+          <label htmlFor="consumo">Indique seu consumo em m³:</label>
+          <input type="number" id="consumo" name="consumo" min="0" max="999999" />
+        </form>
       </div>
     </>
   );
