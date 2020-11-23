8a1c5f6eb44dc59b99bbfcad9780667e23200f25
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2020-11-23 19:23:50 -0300

    Colocando link para pagina inicial na navbar e adicionando margin embaixo da div da calculadora.

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,11 +1,18 @@
 import Image from 'next/image';
+import Link from 'next/link';
+
 
 import styles from '../../styles/Navbar.module.css';
 
 export default function Navbar() {
   return (
     <header className={styles.navbar}>
-      <Image className="clicavel" src="/gota-azul.png" alt="Logo Gota" width={120} height={68.89} />
+      <Link href="/">
+        <a>
+          <Image className="clicavel" src="/gota-azul.png" alt="Logo Gota" width={120} height={68.89} />
+        </a>
+      </Link>
+
     </header>
   );
 }
