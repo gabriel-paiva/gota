c1f1e4ce27385df6e850a11eef0c25e7c9322583
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2020-11-24 17:40:05 -0300

    package.json

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { User, LogIn, LogOut, Settings, PlusSquare } from 'react-feather';
+
 
 import styles from '../../styles/Navbar.module.css';
 
@@ -12,7 +14,31 @@ export default function Navbar() {
           <Image className="clicavel" src="/gota-azul.png" alt="Logo Gota" width={120} height={68.89} />
         </a>
       </Link>
-
+      <Link href="/userpage">
+        <a>
+          <User color="#6E9DC9" size={24} />
+        </a>
+      </Link>
+      <Link href="/profile">
+        <a>
+          <Settings color="#6E9DC9" size={24} />
+        </a>
+      </Link>
+      {/* <Link href="/">
+        <a>
+          <PlusSquare color="#6E9DC9" size={24} />
+        </a>
+      </Link> */}
+      <Link href="/login">
+        <a>
+          <LogIn color="#6E9DC9" size={24} />
+        </a>
+      </Link>
+      {/* <Link href="#">
+        <a>
+          <LogOut color="#6E9DC9" size={24} />
+        </a>
+      </Link> */}
     </header>
   );
 }
