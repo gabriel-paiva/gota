c006bda98a340ef98d2218feb04f0615138dcaec
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2020-11-25 22:21:06 -0300

    Mudando formato de apresentaçao das datas de atualizaçao dos dados

--- components/Resultado/index.js
+++ components/Resultado/index.js
@@ -1,6 +1,10 @@
 import styles from '../../styles/Resultado.module.css';
 
 export default function Resultado(props) {
+
+  const ultimaModificacaoBarra = props.ultimaModificacao.replace(/-/g, '/');
+  const validoAteBarra = props.validoAte.replace(/-/g, '/');
+
   return (
     <div id="resultado" className={styles.resultadoDiv}>
       <h2>Resultado:</h2>
@@ -11,8 +15,8 @@ export default function Resultado(props) {
       <br/>
       <h2>Aviso:</h2>
       <p>Esse cálculo foi feito com base em dados disponibilizados pelas empresas de água e saneamento e coletados manualmente.</p>
-      <p>Ultima atualização: {props.ultimaModificacao}</p>
-      <p>Válidos até: {props.validoAte}</p>
+      <p>Ultima atualização: {ultimaModificacaoBarra}</p>
+      <p>Válidos até: {validoAteBarra}</p>
     </div>
   );
 }
