61a0050b6463d577bbb6e5a0f46a428e7dd614a9
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2020-11-26 10:48:24 -0300

    Renderizando botoes da navbar de acordo com o login do usuario

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { User, LogIn, LogOut, Settings, PlusSquare } from 'react-feather';
 
@@ -7,6 +8,20 @@ import { User, LogIn, LogOut, Settings, PlusSquare } from 'react-feather';
 import styles from '../../styles/Navbar.module.css';
 
 export default function Navbar() {
+
+  const router = useRouter()
+
+  let isLoged = false
+
+  if (typeof window !== 'undefined') {
+    isLoged = localStorage.getItem('isLoged');
+  }
+
+  function handleLogout(){
+    localStorage.clear();
+    router.push('/');
+  }
+
   return (
     <header className={styles.navbar}>
       <Link href="/">
@@ -14,31 +29,35 @@ export default function Navbar() {
           <Image className="clicavel" src="/gota-azul.png" alt="Logo Gota" width={120} height={68.89} />
         </a>
       </Link>
-      <Link href="/userpage">
-        <a>
-          <User color="#6E9DC9" size={24} />
-        </a>
-      </Link>
-      <Link href="/profile">
-        <a>
-          <Settings color="#6E9DC9" size={24} />
-        </a>
-      </Link>
+      {isLoged &&
+        <Link href="/userpage">
+          <a>
+            <User color="#6E9DC9" size={24} />
+          </a>
+        </Link>}
+      {isLoged &&
+        <Link href="/profile">
+          <a>
+            <Settings color="#6E9DC9" size={24} />
+          </a>
+        </Link>}
       {/* <Link href="/">
         <a>
           <PlusSquare color="#6E9DC9" size={24} />
         </a>
       </Link> */}
-      <Link href="/login">
-        <a>
-          <LogIn color="#6E9DC9" size={24} />
-        </a>
-      </Link>
-      {/* <Link href="#">
-        <a>
-          <LogOut color="#6E9DC9" size={24} />
-        </a>
-      </Link> */}
+      {!isLoged &&
+        <Link href="/login">
+          <a>
+            <LogIn color="#6E9DC9" size={24} />
+          </a>
+        </Link>}
+      {isLoged &&
+        <Link href="#" >
+          <a>
+            <LogOut color="#6E9DC9" size={24} onClick={handleLogout} />
+          </a>
+        </Link>}
     </header>
   );
 }
