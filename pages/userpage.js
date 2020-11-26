40723aefeecb04543c52155dbe118ecc95438996
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2020-11-26 13:55:50 -0300

    Criando esqueleto basico da pagina de usuario

--- pages/userpage.js
+++ pages/userpage.js
@@ -1,3 +1,5 @@
+import styles from '../styles/Userpage.module.css';
+
 import Navbar from '../components/Navbar';
 
 export default function Userpage() {
@@ -5,7 +7,105 @@ export default function Userpage() {
     <>
       <Navbar />
       <div className="container">
-        <h1>Página de Usuários lindíssima</h1>
+        <h1>Central do Usuário</h1>
+
+        <div className={styles.formdiv}>
+          <h2>Cadastrar conta</h2>
+          <form>
+            <div className={styles.inputdiv}>
+              <label htmlFor="data">Data:</label>
+              <input
+                type="date"
+                name="data"
+                id="data"
+                placeholder="Insira a data da sua conta"
+                required
+                autoFocus
+              />
+            </div>
+
+            <div className={styles.inputdiv}>
+              <label htmlFor="valor">Valor (R$):</label>
+              <input
+                type="number"
+                name="valor"
+                id="valor"
+                step=".01"
+                min="0.00"
+                max="999999"
+                placeholder="Insira o valor da sua conta"
+                required
+              />
+            </div>
+
+            <div className={styles.inputdiv}>
+              <label htmlFor="consumo">Consumo (m³):</label>
+              <input
+                type="number"
+                name="consumo"
+                id="consumo"
+                step="1"
+                min="0"
+                max="999999"
+                placeholder="Insira o consumo da sua conta"
+                required
+              />
+            </div>
+
+            <div className={styles.inputdiv} >
+              <label htmlFor="empresa">Empresa de saneamento:</label>
+              <select
+                name="empresa"
+                id="empresa"
+                required
+              >
+                <option>Escolha uma empresa de saneamento</option>
+                <option>Águas Guariroba</option>
+                <option>Caesb</option>
+                <option>Saneago</option>
+                <option>Sanesul</option>
+              </select>
+            </div>
+
+            <div className={styles.inputdiv} >
+              <label htmlFor="categoria">Categoria:</label>
+              <select
+                name="categoria"
+                id="categoria"
+                required
+              >
+                <option>Escolha a categoria da sua conta</option>
+                <option>Residencial Padrão</option>
+                <option>Residencial Social</option>
+                <option>Público</option>
+                <option>Industrial</option>
+              </select>
+            </div>
+
+            <div className={styles.inputdiv} >
+              <label htmlFor="status">Status:</label>
+              <select
+                name="status"
+                id="status"
+                required
+              >
+                <option>Defina o status da sua conta</option>
+                <option>Pago</option>
+                <option>Pendente</option>
+              </select>
+            </div>
+            <button type="submit" className="clicavel">Salvar</button>
+          </form>
+        </div>
+
+        <div className={styles.formdiv}>
+          <h2>Contas salvas</h2>
+        </div>
+
+        <div className={styles.formdiv}>
+          <h2>Relatórios</h2>
+        </div>
+
       </div>
     </>
   );
